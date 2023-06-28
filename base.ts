f57cfd3442285b7aeb10b21b67a5190d8406886a
The0Show <62229104+The0Show@users.noreply.github.com>
2023-06-28 02:18:14 -0400

    Make things sound less... "harsh"?

--- base.ts
+++ base.ts
@@ -1,11 +1,11 @@
 // The const name SHOULD match the file name.
 const base = {
-	name: "Translation Name", // This should be in the native language. For example, "Spanish" is "Español"
-	emoji: "🍌", // This should be the flag of the native country using this language.
-	authors: ["you!"], // (Markdown is supported!)
+	name: "Translation Name", // This should be in the language. For example, "Spanish" would be "Español"
+	emoji: "🍌", // This should really be a country flag, but if there is none use an emoji that represents the locale.
+	authors: ["you!"], // (Markdown is supported - feel free to add links)
 	keys: {
 		// Put the keys here! any keys that aren't defined will fallback to en-US
 	},
 };
 
-export default base; // Ensure that your const is defined!
+export default base; // Ensure that your const is exported!
